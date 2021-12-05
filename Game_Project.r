ddafeea2c49698e1c583fae978f1b53632be1e53
Cole Wood <78818613+wood0639@users.noreply.github.com>
2021-12-05 17:23:19 -0600

    Update Game_Project.r

--- Game_Project.r
+++ Game_Project.r
@@ -1,11 +1,17 @@
+# Create Dataframe
 game <- data.frame(steam_charts)
 
+# Fix date format 
 game <- subset(game, Month!="Last 30 Days")
 game1 <- sub(" ", " 01 ", game$Month)
 game$Month <- game1
 
-
-
 game$Month <- as.Date(game$Month, "%B %d %Y"); game$Month
+
+# Show date type
 class(game$Month)
+
+# Clean up column names
+cnames <- c("Month","AvgPlayer","Gain","PercentGain","PeakPlayers","AppID","Game")
+colnames(game) <- cnames
 
