390cd844c9c354f046c7529de3cbb3a2ed1f0053
trsain <93837140+trsain@users.noreply.github.com>
2021-12-18 20:31:41 -0500

    Update Game_Project.r
    Not sure if the file I was able to write is useful for the project but it is an actual thing in r :)

--- Game_Project.r
+++ Game_Project.r
@@ -87,3 +87,92 @@ toksNoStops.Bottom <- tokens_select(toks.Bottom, pattern = stopwords("english"),
 gameDFM.Bottom <- dfm(toksNoStops.Bottom, remove = c("Ã", "Â", "¯", "¿", "½", "¢", "®", "é", "»", "'", "ç", ">", "S", "Y", "å", "T", "¨"))
 
 gameWC.Bottom <- textplot_wordcloud(gameDFM.Bottom, min_count = 2)
+
+
+#For each game average # of peak players?
+#group the data by APPID and then PeakPlayers
+PPGroupBy =
+game %>% group_by(Game,PeakPlayers)
+#summarise the data into summary
+summary <- summarise(PPGroupBy)
+#find the average and store in variable
+average_peak_pergame <- with(summary, by(PeakPlayers, Game, mean))
+#put back into a matrix
+average_peak_pergame_2 <- t(sapply(average_peak_pergame, I))
+#create data frame for avg peak per game
+new_avg_peak.df <- as.data.frame(average_peak_pergame_2)
+#stack two create two columns
+df <- stack(new_avg_peak.df)
+df <- na.omit(df)
+#update the column names
+colnames(df) <- c('AvgPeakPlayers','Game')
+#store into new dataframe and order in ascending order
+newdf <- df[order(df$AvgPeakPlayers),]
+#set top 10 and lowest 10 to veriables
+low <- head(newdf, n = 10)
+high <- tail(newdf, n = 10)
+
+#visuals
+#create bar graph for top 10 lowest 10 avg Peak Players
+p1<-ggplot(data=low, aes(x=Game, y=AvgPeakPlayers)) +
+geom_bar(stat="identity", show.legend = FALSE,  fill = "#0072B2")+
+theme_classic()
+p1+coord_flip()
+p2<-ggplot(data=high, aes(x=Game, y=AvgPeakPlayers)) +
+geom_bar(stat="identity", show.legend = FALSE,  fill = "#0072B2")+
+theme_classic()
+p2+coord_flip()
+
+#write a file with all the average Peak Players per Game, not sure if this is useful for the project
+write.csv(newdf, file = 'avg_peakplayers_pergame.csv')
+
+
+#Is there a specific season or month where the number of players increases
+#the most on average?
+
+#create a column with just month store in a new column name
+game$MonthAbrev <- format(game$Month, format="%b")
+game$Year <- format(game$Month, format="%Y")
+#group by month, year,  and average players
+SeasonsData =
+game %>% group_by(MonthAbrev,Year,AvgPlayer)
+#summarise the data into summary
+summary2 <- summarise(SeasonsData)
+#total amount of players per year
+totalperyear <- with(summary2, by(AvgPlayer, Year, sum))
+#put back into a matrix
+totalperyear2 <- t(sapply(totalperyear, I))
+#create data frame for avg peak per game
+totalperyear.df <- as.data.frame(totalperyear2)
+#stack two create two columns
+df2 <- stack(totalperyear.df)
+#update the column names
+colnames(df2) <- c('Total Avg Players','Year')
+#create column with month and year together
+game$MonYear <- format(game$Month, format="%b %Y")
+#group by month, year,  and average players
+SeasonsData2 =
+game %>% group_by(MonYear,AvgPlayer)
+#summarise the data into summary
+summary3 <- summarise(SeasonsData2)
+#total amount of players per month per year
+totalpermonth <- with(summary3, by(AvgPlayer, MonYear, sum))
+#put back into a matrix
+totalpermonth2 <- t(sapply(totalpermonth, I))
+#create data frame for avg peak per game
+totalpermonth.df <- as.data.frame(totalpermonth2)
+#stack two create two columns
+df3 <- stack(totalpermonth.df)
+#update the column names
+colnames(df3) <- c('TotalAvgPlayers','MonthYear')
+#visulizations
+p3<-ggplot(data=df3, aes(x=MonthYear, y=TotalAvgPlayers)) +
+geom_bar(stat="identity",show.legend = FALSE, fill = '#009E73')+
+theme_classic()+
+theme(axis.text.x = element_text(angle = 90))
+p3
+#display settings
+require(scales)
+p3 + scale_y_continuous(labels = comma)
+View(p3)
+
